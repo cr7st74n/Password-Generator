67787746641ad10d11a61e5f40ef8247764bb8f2
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2022-06-27 12:28:41 -0400

    update

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  //Create a funtion to validate and get the leght of characters for my password
+  function promptNumCharacters(){
+    var NumCharacters = prompt("Enter the leght of character that you wish !");
+    while (NumCharacters < 8 || NumCharacters > 128){
+      alert("Your number should be greater than 8 and less than 128");
+      NumCharacters = prompt("Try agrain.!");
+    }
+    return NumCharacters;
+  }
+
+
+
+  var ArrayQuestions = ["lowercase","uppercase","numeric","special character"];
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
